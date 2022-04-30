88a42102e040c8220682d62767600722536a6548
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-04-29 20:43:04 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 echo Installing Spigot...
 
+mkdir Spigot
+cd Spigot
+
 wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 java -jar BuildTools.jar --disable-java-check --rev 1.18.2 --remapped
+
+cd ..
 
 echo Installing Paper...
 
@@ -9,5 +14,7 @@ git clone https://github.com/PaperMC/Paper.git
 cd Paper
 ./gradlew applyPatches
 ./gradlew publishToMavenLocal
+
+cd ..
 
 echo Done
