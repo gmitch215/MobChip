d8dbbb37055ae746b24ab20049eccdee6f00d518
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-05-01 01:13:37 -0500

    Update javadoc.sh

--- javadoc.sh
+++ javadoc.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-echo "[MobChip JavaDoc Builder] Starting..."
-rm -rf docs/bukkit && rm -rf docs/base && rm -rf docs/paper && mkdir docs/base && mkdir docs/bukkit && mkdir docs/paper
-echo "[MobChip JavaDoc Builder] Injecting..."
-cp -R mobchip-base/target/apidocs/* docs/base && cp -R mobchip-bukkit/target/apidocs/* docs/bukkit && cp -R mobchip-paper/target/apidocs/* docs/paper
-echo "[MobChip JavaDoc Builder] Done!"
+echo "[MobChip JavaDoc Builder] Starting..." && rm -rf docs/bukkit && rm -rf docs/base && rm -rf docs/paper && mkdir docs/base && mkdir docs/bukkit && mkdir docs/paper && echo "[MobChip JavaDoc Builder] Injecting..." && cp -R mobchip-base/target/apidocs/* docs/base && cp -R mobchip-bukkit/target/apidocs/* docs/bukkit && cp -R mobchip-paper/target/apidocs/* docs/paper && echo "[MobChip JavaDoc Builder] Done!"
