c890ed46e1571acdbc4e3ef4e444272ca25f1eae
GamerCoder215 <gamercoder215@gmail.com>
2022-05-01 11:22:49 -0500

    Update NavigationNode.java
    JavaDocs

--- mobchip-base/src/main/java/me/gamercoder215/mobchip/ai/navigation/NavigationNode.java
+++ mobchip-base/src/main/java/me/gamercoder215/mobchip/ai/navigation/NavigationNode.java
@@ -17,41 +17,85 @@ public final class NavigationNode implements Cloneable {
     private int x;
     private int y;
     private int z;
-
+    
+    /**
+     * Constructs a NavigationNode at this Entity.
+     * @param en Entity to use
+     */
     public NavigationNode(@NotNull Entity en) {
         this(en.getLocation());
     }
-
+    
+    /**
+     * Constructs a NavigationNode at this Location.
+     * @param loc Location
+     */
     public NavigationNode(@NotNull Location loc) {
         this(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
     }
-
+    
+    /**
+     * Constructs a NavigationNode at these coordinates.
+     * @param x X coord
+     * @param y Y Coord
+     * @param z Z coord
+     */
     public NavigationNode(int x, int y, int z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
-
+    
+    /**
+     * Gets the distance of this NavigationNode to another NavigationNode.
+     * @param node NavigationNode to use
+     * @return distance
+     */
     public float distance(@NotNull NavigationNode node) {
         return getHandle().distanceTo(node.getHandle());
     }
-
+    
+    /**
+     * Gets the distance of this NavigationNode to another Location.
+     * @param loc Location to use
+     * @return distance
+     */
     public float distance(@NotNull Location loc) {
         return getHandle().distanceTo(ChipConversions.convertType(loc));
     }
-
+    
+    /**
+     * Gets the distance of this NavigationNode to another Entity.
+     * @param en Entity to use
+     * @return distance
+     */
     public float distance(@NotNull Entity en) {
         return distance(en.getLocation());
     }
-
+    
+    /**
+     * Gets the distance, squared, of this NavigationNode to another NavigationNode.
+     * @param node NavigationNode to use
+     * @return distance squared
+     */
     public float distanceSquared(@NotNull NavigationNode node) {
         return getHandle().distanceToSqr(node.getHandle());
     }
-
+    
+    /**
+     * Gets the distance, squared, of this NavigationNode to another Location.
+     * @param loc Location to use
+     * @return distance squared
+     */
     public float distanceSquared(@NotNull Location loc) {
         return getHandle().distanceToSqr(ChipConversions.convertType(loc));
     }
-
+    
+    /**
+     * Gets the distance, squared, of this NavigationNode to another Entity.
+     * @param en Entity to use
+     * @return distance squared
+     */
     public float distanceSquared(@NotNull Entity en) {
         return distance(en.getLocation());
     }
@@ -63,15 +107,27 @@ public final class NavigationNode implements Cloneable {
     public Node getHandle() {
         return new Node(x, y, z);
     }
-
+    
+    /**
+     * Fetches the current X value of this NavigationNode.
+     * @return X coordinate
+     */
     public int getX() {
         return this.x;
     }
-
+    
+    /**
+     * Fetches the current Y value of this NavigationNode.
+     * @return Y coordinate
+     */
     public int getY() {
         return this.y;
     }
-
+    
+    /**
+     * Fetches the current Z value of this NavigationNode.
+     * @return Z coordinate
+     */
     public int getZ() {
         return this.z;
     }
@@ -103,7 +159,13 @@ public final class NavigationNode implements Cloneable {
         this.z -= z;
         return this;
     }
-
+    
+    /**
+     * Converts this NavigationNode to a Bukkit Location.
+     * @param w World to use
+     * @return Bukkit Location created
+     */
+    @NotNull
     public Location toLocation(@Nullable World w) {
         return new Location(w, x, y, z);
     }
