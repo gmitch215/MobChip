3faa74b7e3c37f7c3a6c5e1581f122b1f8143db0
GamerCoder215 <gamercoder215@gmail.com>
2022-05-22 12:49:48 -0500

    Update Javadocs

--- javadoc.sh
+++ javadoc.sh
@@ -1,2 +1,42 @@
 #!/bin/bash
-echo "[MobChip JavaDoc Builder] Starting..." && rm -rf docs/bukkit && rm -rf docs/base && mkdir docs/base && mkdir docs/bukkit && echo "[MobChip JavaDoc Builder] Injecting..." && cp -R mobchip-base/target/apidocs/* docs/base && cp -R mobchip-bukkit/target/apidocs/* docs/bukkit && echo "[MobChip JavaDoc Builder] Committing..." && git config --local user.email "action@github.com" && git config --local user.name "GitHub Actions" && git add docs/bukkit && git add docs/base && git commit -m "Update Javadocs" && git push && echo "[MobChip JavaDoc Builder] Done!"
+
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Actions"
+git fetch origin gh-pages
+
+echo "[MobChip JavaDoc Builder] Starting..."
+
+rm -rf docs/bukkit
+rm -rf docs/base
+
+mkdir ./docs/base
+mkdir ./docs/bukkit
+
+echo "[MobChip JavaDoc Builder] Injecting..."
+
+cp -R mobchip-base/target/apidocs/* docs/base
+cp -R mobchip-bukkit/target/apidocs/* docs/bukkit
+
+git checkout gh-pages
+
+for dir in ./*
+do
+  if [ "$dir" == "./docs" ]; then
+    continue
+  fi
+
+  rm -rf "$dir"
+done
+
+cp -Rfv ./docs/* ./
+rm -rf ./docs
+
+echo "[MobChip JavaDoc Builder] Committing..."
+
+git add .
+git branch -D gh-pages
+git branch -m gh-pages
+git commit -m "Update Javadocs"
+git push -f origin gh-pages
+
+echo "[MobChip JavaDoc Builder] Done!"
