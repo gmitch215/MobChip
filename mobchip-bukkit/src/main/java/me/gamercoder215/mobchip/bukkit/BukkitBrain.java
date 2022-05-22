b9039b1fbf77b791c4adbdbf941079d927763e04
GamerCoder215 <gamercoder215@gmail.com>
2022-05-22 13:29:16 -0500

    Update BukkitBrain.java
    - Copy JavaDocs
    - Implement clearRestrictionArea()

--- mobchip-bukkit/src/main/java/me/gamercoder215/mobchip/bukkit/BukkitBrain.java
+++ mobchip-bukkit/src/main/java/me/gamercoder215/mobchip/bukkit/BukkitBrain.java
@@ -40,24 +40,47 @@ public final class BukkitBrain implements EntityBrain {
 		return new BukkitBrain(m);
 	}
 
+	/**
+	 * Get the Entity AI associated with this Brain.
+	 * @return Entity AI
+	 */
 	public @NotNull EntityAI getGoalAI() {
 		return new BukkitAI(nmsMob.goalSelector, false);
 	}
 
+	/**
+	 * Get the Entity Target AI associated with this Brain.
+	 * @return Entity Target AI
+	 */
 	public @NotNull EntityAI getTargetAI() {
 		return new BukkitAI(nmsMob.targetSelector, true);
 	}
 
+	/**
+	 * Creates new Entity Navigation associated with this Brain.
+	 * @return Created Entity Navigation
+	 */
 	@Override
 	public @NotNull EntityNavigation createNavigation() {
 		return new BukkitNavigation(nmsMob.getNavigation(), m);
 	}
 
+	/**
+	 * Get the Entity Controller associated with this Brain.
+	 * @return Entity Controller
+	 */
 	@Override
 	public @NotNull EntityController getController() {
 		return new BukkitController(m.getWorld(), nmsMob.getJumpControl(), nmsMob.getLookControl(), nmsMob.getMoveControl());
 	}
 
+	/**
+	 * Sets a permanent memory into this entity's brain.
+	 * @param memory Memory to change
+	 * @param value Value of new memory, null to remove
+	 * @throws IllegalArgumentException if the value is not suitable for this memory
+	 * @param <T> Memory Type
+	 */
 	@Override
 	public <T> void setMemory(@NotNull EntityMemory<T> memory, @Nullable T value) throws IllegalArgumentException {
 		if (value == null) {
@@ -75,6 +98,16 @@ public final class BukkitBrain implements EntityBrain {
 		Bukkit.getPluginManager().callEvent(event);
 	}
 
+	/**
+	 * Sets a temporary memory into this entity's brain.
+	 * <p>
+	 * Removing ANY memory should be using {@link #setMemory(EntityMemory, Object)} with null as the second parameter.
+	 * @param memory Memory to change
+	 * @param value Value of new memory
+	 * @param expire How many ticks until this memory will be forgotten/removed
+	 * @throws IllegalArgumentException if the value is not suitable for this memory / ticks amount is invalid
+	 * @param <T> Memory Type
+	 */
 	@Override
 	public <T> void setMemory(@NotNull EntityMemory<T> memory, @Nullable T value, long expire) throws IllegalArgumentException {
 		if (expire < 0) throw new IllegalArgumentException("Invalid ticks number " + expire);
@@ -87,6 +120,12 @@ public final class BukkitBrain implements EntityBrain {
 		Bukkit.getPluginManager().callEvent(event);
 	}
 
+	/**
+	 * Fetch the Memory that is stored in this Entity's Brain.
+	 * @param memory Memory to fetch
+	 * @return Found value as an object, null if not present
+	 * @param <T> Memory Type
+	 */
 	@Override
 	public <T> @Nullable T getMemory(@NotNull EntityMemory<T> memory) {
 		if (nmsMob.getBrain().getMemory(memory.getHandle()).isPresent())
@@ -94,33 +133,63 @@ public final class BukkitBrain implements EntityBrain {
 		else return null;
 	}
 
+	/**
+	 * Get the expiration date of this Memory.
+	 * @param memory Memory to fetch
+	 * @return Found expiration date, or 0 if no expiration or not found
+	 */
 	@Override
-
 	public long getExpiration(@NotNull EntityMemory<?> memory) {
 		return nmsMob.getBrain().getTimeUntilExpiry(memory.getHandle());
 	}
 
+	/**
+	 * Whether this Brain contains this memory.
+	 * @param memory Memory to fetch
+	 * @return true if contains, else false
+	 */
 	@Override
 	public boolean containsMemory(@NotNull EntityMemory<?> memory) {
 		return nmsMob.getBrain().hasMemoryValue(memory.getHandle());
 	}
 
+	/**
+	 * Whether this Entity is in its restriction area.
+	 * @return true if inside, else false
+	 */
 	@Override
 	public boolean isInRestriction() {
 		return nmsMob.isWithinRestriction();
 	}
 
+	/**
+	 * Sets the Restriction Area for this Entity.
+	 * @param center Location center
+	 * @param radius Radius of restriction center
+	 */
 	@Override
-	public void setRestrictionArea(Location c, int r) {
-		RestrictionSetEvent event = new RestrictionSetEvent(this, getRestrictionArea(), c, getRestrictionRadius(), r);
+	public void setRestrictionArea(Location center, int radius) {
+		RestrictionSetEvent event = new RestrictionSetEvent(this, getRestrictionArea(), center, getRestrictionRadius(), radius);
 		Bukkit.getPluginManager().callEvent(event);
 
-		Location center = event.getNewCenter();
-		int radius = event.getNewRadius();
+		Location newCenter = event.getNewCenter();
+		int newRadius = event.getNewRadius();
 
-		nmsMob.restrictTo(new BlockPos(center.getX(), center.getY(), center.getZ()), radius);
+		nmsMob.restrictTo(new BlockPos(newCenter.getX(), newCenter.getY(), newCenter.getZ()), newRadius);
 	}
 
+	/**
+	 * Clears this Entity's Restriction Area.
+	 */
+	@Override
+	public void clearRestrictionArea() {
+		nmsMob.clearRestriction();
+	}
+
+	/**
+	 * Gets the current restriction area.
+	 * @return Restriction Area
+	 */
 	@Override
 	public Location getRestrictionArea() {
 		BlockPos center = nmsMob.getRestrictCenter();
@@ -128,21 +197,38 @@ public final class BukkitBrain implements EntityBrain {
 		return new Location(m.getWorld(), center.getX(), center.getY(), center.getZ());
 	}
 
+	/**
+	 * Whether this entity has a restriction area.
+	 * @return true if it has a restriction area, else false
+	 */
 	@Override
 	public boolean hasRestriction() {
 		return nmsMob.hasRestriction();
 	}
 
+	/**
+	 * Fetch the current restriction radius for this entity.
+	 * @return Restriction Radius
+	 */
 	@Override
 	public int getRestrictionRadius() {
 		return Math.min((int) Math.floor(nmsMob.getRestrictRadius()), Integer.MAX_VALUE);
 	}
 
+	/**
+	 * Whether this Mob can see another Entity.
+	 * @param en Entity to test
+	 * @return true if entity can see, else false
+	 */
 	@Override
 	public boolean canSee(@Nullable Entity en) {
 		return nmsMob.getSensing().hasLineOfSight(ChipConversions.convertType(en));
 	}
 
+	/**
+	 * Get the Entity that this Brain relates to.
+	 * @return Entity of this brain
+	 */
 	@Override
 	public @NotNull Mob getEntity() {
 		return this.m;
