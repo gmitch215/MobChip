e698b7d6d4a5a483dfa1e0ad653d987cdcfcdbcd
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-06-19 22:26:11 -0500

    Update BukkitAI.java
    Move Goal Methods

--- bukkit/src/main/java/me/gamercoder215/mobchip/bukkit/BukkitAI.java
+++ bukkit/src/main/java/me/gamercoder215/mobchip/bukkit/BukkitAI.java
@@ -6,7 +6,6 @@ import me.gamercoder215.mobchip.ai.goal.PathfinderInfo;
 import me.gamercoder215.mobchip.bukkit.events.pathfinder.PathfinderAddEvent;
 import me.gamercoder215.mobchip.bukkit.events.pathfinder.PathfinderClearEvent;
 import me.gamercoder215.mobchip.bukkit.events.pathfinder.PathfinderRemoveEvent;
-import me.gamercoder215.mobchip.util.MobChipUtil;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.goal.GoalSelector;
 import net.minecraft.world.entity.ai.goal.WrappedGoal;
@@ -14,6 +13,10 @@ import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.net.URL;
 import java.util.*;
 
 final class BukkitAI implements EntityAI {
@@ -21,7 +24,86 @@ final class BukkitAI implements EntityAI {
 	private final Map<Integer, Goal> goals = new HashMap<>();
 	private final GoalSelector sel;
 	private final boolean target;
-	
+
+	static Pathfinder wrapGoal(Goal g) {
+		try {
+			Constructor<? extends Pathfinder> constr = wrapGoal(g.getClass()).getConstructor(g.getClass());
+			constr.setAccessible(true);
+			return constr.newInstance(g);
+		} catch (ExceptionInInitializerError e) {
+			return null;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	static Class<? extends Pathfinder> wrapGoal(Class<? extends Goal> class1) {
+		try {
+			if (class1.equals(Goal.class)) return Pathfinder.class;
+			for (Class<? extends Pathfinder> clazz : getPathfinders()) {
+				if (clazz.getDeclaredMethod("getHandle").getReturnType().getCanonicalName().equals(class1.getCanonicalName())) return clazz;
+			}
+			return null;
+		} catch (Exception e) {
+			return Pathfinder.class;
+		}
+	}
+
+	static List<Class<? extends Pathfinder>> getPathfinders() {
+		try {
+			Class<?>[] arr1 = getClasses("me.gamercoder215.mobchip.ai.goal");
+			Class<?>[] arr2 = getClasses("me.gamercoder215.mobchip.ai.goal.target");
+
+			List<Class<? extends Pathfinder>> list = new ArrayList<>();
+			for (Class<?> clazz : arr1) try {
+				list.add(clazz.asSubclass(Pathfinder.class));
+			} catch (ClassCastException ignored) {
+			}
+			for (Class<?> clazz : arr2) try {
+				list.add(clazz.asSubclass(Pathfinder.class));
+			} catch (ClassCastException ignored) {
+			}
+
+			return list;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ArrayList<>();
+		}
+	}
+
+	private static Class<?>[] getClasses(String packageName) throws ClassNotFoundException, IOException {
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		String path = packageName.replace('.', '/');
+		Enumeration<URL> resources = classLoader.getResources(path);
+		List<File> dirs = new ArrayList<>();
+		while (resources.hasMoreElements()) {
+			URL resource = resources.nextElement();
+			dirs.add(new File(resource.getFile()));
+		}
+		List<Class<?>> classes = new ArrayList<>();
+		for (File directory : dirs) {
+			classes.addAll(findClasses(directory, packageName));
+		}
+		return classes.toArray(new Class[0]);
+	}
+
+	private static List<Class<?>> findClasses(File directory, String packageName) throws ClassNotFoundException {
+		List<Class<?>> classes = new ArrayList<>();
+		if (!directory.exists()) {
+			return classes;
+		}
+		File[] files = directory.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				classes.addAll(findClasses(file, packageName + "." + file.getName()));
+			} else if (file.getName().endsWith(".class")) {
+				classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+			}
+		}
+		return classes;
+	}
+
 	protected BukkitAI(GoalSelector sel, boolean target) {
 		this.sel = sel;
 		this.target = target;
@@ -66,7 +148,7 @@ final class BukkitAI implements EntityAI {
 	@Override
 	public Pathfinder get(Object key) {
 		if (!(key instanceof Integer in)) return null;
-		return MobChipUtil.wrapGoal(goals.get(in));
+		return wrapGoal(goals.get(in));
 	}
 
 	@Override
@@ -89,7 +171,7 @@ final class BukkitAI implements EntityAI {
 	public Pathfinder remove(Object key) {
 		Goal g = goals.remove(key);
 		updateAI();
-		Pathfinder p = MobChipUtil.wrapGoal(g);
+		Pathfinder p = wrapGoal(g);
 		PathfinderRemoveEvent event = new PathfinderRemoveEvent(this, p, this.target);
 		Bukkit.getPluginManager().callEvent(event);
 		return p;
@@ -124,7 +206,7 @@ final class BukkitAI implements EntityAI {
 	public @NotNull List<Pathfinder> values() {
 		List<Pathfinder> values = new ArrayList<>();
 		for (WrappedGoal g : sel.getAvailableGoals()) {
-			values.add(MobChipUtil.wrapGoal(g.getGoal()));
+			values.add(wrapGoal(g.getGoal()));
 		}
 
 		return values;
@@ -134,7 +216,7 @@ final class BukkitAI implements EntityAI {
 	public @NotNull Set<Map.Entry<Integer, Pathfinder>> entrySet() {
 		Set<Map.Entry<Integer, Pathfinder>> entries = new HashSet<>();
 		for (WrappedGoal g : sel.getAvailableGoals()) {
-			entries.add(new AbstractMap.SimpleEntry<>(g.getPriority(), MobChipUtil.wrapGoal(g.getGoal())));
+			entries.add(new AbstractMap.SimpleEntry<>(g.getPriority(), wrapGoal(g.getGoal())));
 		}
 
 		return entries;
@@ -147,7 +229,7 @@ final class BukkitAI implements EntityAI {
 	@Override
 	public @NotNull Set<Pathfinder> getRunningGoals() {
 		Set<Pathfinder> running = new HashSet<>();
-		sel.getRunningGoals().forEach(r -> running.add(MobChipUtil.wrapGoal(r.getGoal())));
+		sel.getRunningGoals().forEach(r -> running.add(wrapGoal(r.getGoal())));
 		return running;
 	}
 
