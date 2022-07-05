8a372a8c3ed50b5389f1f6fb570334acaabc8548
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-07-04 20:51:28 -0500

    Update 1.19 Wrappers
    - Move Frog Behavior, Warden Behavior

---
+++ 1_19_R1/src/main/java/me/gamercoder215/mobchip/bukkit/BukkitFrogBehavior.java
@@ -0,0 +1,22 @@
+package me.gamercoder215.mobchip.bukkit;
+
+import me.gamercoder215.mobchip.ai.behavior.BehaviorResult;
+import me.gamercoder215.mobchip.ai.behavior.FrogBehavior;
+import org.bukkit.Sound;
+import org.bukkit.entity.Frog;
+import org.jetbrains.annotations.NotNull;
+
+class BukkitFrogBehavior extends BukkitCreatureBehavior implements FrogBehavior {
+
+    final Frog m;
+
+    public BukkitFrogBehavior(Frog f) {
+        super(f);
+        this.m = f;
+    }
+
+    @Override
+    public @NotNull BehaviorResult shootTongue(Sound tongueSound, Sound eatSound) {
+        return run("ShootTongue");
+    }
+}
