3724566e85bcad88ff39cc704f87fe318098b62e
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-07-04 21:01:48 -0500

    Update EntityAI.java

--- base/src/main/java/me/gamercoder215/mobchip/ai/EntityAI.java
+++ base/src/main/java/me/gamercoder215/mobchip/ai/EntityAI.java
@@ -1,23 +1,23 @@
 package me.gamercoder215.mobchip.ai;
 
 import me.gamercoder215.mobchip.ai.goal.Pathfinder;
+import me.gamercoder215.mobchip.ai.goal.WrappedPathfinder;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Map;
 import java.util.Set;
 
 /**
  * Represents Entity Pathfinder AI 
  */
-public interface EntityAI extends Map<Integer, Pathfinder> {
+public interface EntityAI extends Set<WrappedPathfinder> {
 
     /**
      * Fetches all pathfinders that this Entity is running.
      * @return Set of running Pathfinders
      */
     @NotNull
-    Set<Pathfinder> getRunningGoals();
+    Set<WrappedPathfinder> getRunningGoals();
 
     /**
      * Disables all Pathfinders with this flag.
