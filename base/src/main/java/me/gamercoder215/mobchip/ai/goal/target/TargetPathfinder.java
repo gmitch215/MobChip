e947311cb7b7e9a423e87a86ee5aaa94a696cdd2
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-07-04 20:54:21 -0500

    Update TargetPathfinder.java
    Update Constructor

--- base/src/main/java/me/gamercoder215/mobchip/ai/goal/target/TargetPathfinder.java
+++ base/src/main/java/me/gamercoder215/mobchip/ai/goal/target/TargetPathfinder.java
@@ -2,141 +2,63 @@ package me.gamercoder215.mobchip.ai.goal.target;
 
 import me.gamercoder215.mobchip.ai.goal.CustomPathfinder;
 import me.gamercoder215.mobchip.ai.goal.Pathfinder;
-import net.minecraft.world.entity.PathfinderMob;
-import net.minecraft.world.entity.ai.goal.Goal;
-import net.minecraft.world.entity.ai.goal.target.TargetGoal;
 import org.bukkit.entity.Mob;
 import org.jetbrains.annotations.NotNull;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.logging.Logger;
 
 /**
  * Represents a Pathfinder that involves targeting.
  * <p>
  * <strong>For custom Pathfinders, extend {@link CustomPathfinder}.</strong>
  */
-public abstract class TargetPathfinder extends Pathfinder {
+public abstract class TargetPathfinder extends Pathfinder implements Targeting {
 
+    private boolean mustSee;
+    private boolean reach;
 
-	static net.minecraft.world.entity.Mob getEntity(Goal g, String fieldName) {
-		try {
-			
-			Field f = g.getClass().getDeclaredField(fieldName);
-			f.setAccessible(true);
-			
-			Object o = f.get(g);
-			if (o == null && g.getClass().getSuperclass().isAssignableFrom(Goal.class)) o = getEntity((Goal) g.getClass().getSuperclass().cast(g), fieldName);
-			
-			return (net.minecraft.world.entity.Mob) o;
-		} catch (Exception e) {
-			return null;
-		}
-	}
-	
-	static <T> T getField(Goal g, String fieldName, Class<T> clazz) {
-		try {
-			Field f = g.getClass().getDeclaredField(fieldName);
-			f.setAccessible(true);
-			return clazz.cast(f.get(g));
-		} catch (Exception e) {
-			Logger.getGlobal().severe(e.getMessage());
-			return null;
-		}
-	}
-
-	static double getDouble(Goal g, String fieldName) {
-		try {
-			Field f = g.getClass().getDeclaredField(fieldName);
-			f.setAccessible(true);
-			return f.getDouble(g);
-		} catch (Exception e) {
-			Logger.getGlobal().severe(e.getMessage());
-			return 1;
-		}
-	}
-
-	static float getFloat(Goal g, String fieldName) {
-		try {
-			Field f = g.getClass().getDeclaredField(fieldName);
-			f.setAccessible(true);
-			return f.getFloat(g);
-		} catch (Exception e) {
-			Logger.getGlobal().severe(e.getMessage());
-			return 1;
-		}
-	}
-
-	static int getInt(Goal g, String fieldName) {
-		try {
-			Field f = g.getClass().getDeclaredField(fieldName);
-			f.setAccessible(true);
-			return f.getInt(g);
-		} catch (Exception e) {
-			Logger.getGlobal().severe(e.getMessage());
-			return 1;
-		}
-	}
-
-	static void setFinal(Goal g, String fieldName, Object value) {
-		try {
-			Field a = g.getClass().getDeclaredField(fieldName);
-			a.setAccessible(true);
-
-			Field modifiers = Field.class.getDeclaredField("modifiers");
-			modifiers.setAccessible(true);
-			modifiers.setInt(a, a.getModifiers() & ~Modifier.FINAL);
-
-			a.set(g, value);
-		} catch (Exception e) {
-			Logger.getGlobal().severe(e.getMessage());
-		}
-	}
-
-	static boolean getBoolean(Goal g, String fieldName) {
-		try {
-			Field f = g.getClass().getDeclaredField(fieldName);
-			f.setAccessible(true);
-			return f.getBoolean(g);
-		} catch (Exception e) {
-			Logger.getGlobal().severe(e.getMessage());
-			return false;
-		}
-	}
-
-	static PathfinderMob getCreature(Goal g, String fieldName) {
-		try {
-			
-			Field f = g.getClass().getDeclaredField(fieldName);
-			f.setAccessible(true);
-			
-			Object o = f.get(g);
-			if (o == null && g.getClass().getSuperclass().isAssignableFrom(Goal.class)) o = getEntity((Goal) g.getClass().getSuperclass().cast(g), fieldName);
-			
-			return (PathfinderMob) o;
-		} catch (Exception e) {
-			return null;
-		}
-	}
+    /**
+     * Constructs a TargetPathfinder with reach set to false.
+     * @param m Mob to use
+     * @param see Whether the mob has to see the target
+     */
+    protected TargetPathfinder(@NotNull Mob m, boolean see) {
+        this(m, see, false);
+    }
 
     /**
      * Constructs a TargetPathfinder.
      * @param m Mob to use
+     * @param see Whether the mob has to see the target
+     * @param reach Whether the mob has to reach the target
      */
-    protected TargetPathfinder(@NotNull Mob m) {
+    protected TargetPathfinder(@NotNull Mob m, boolean see, boolean reach) {
         super(m);
-    } 
 
-    /**
-     * Constructs a TargetPathfinder.
-     * @param m NMS Mob to use
-     */
-    protected TargetPathfinder(@NotNull net.minecraft.world.entity.Mob m) {
-        super(m);
-    } 
+        this.mustSee = see;
+        this.reach = reach;
+    }
 
     @Override
-    public abstract TargetGoal getHandle();
+    public boolean mustSee() { return this.mustSee; }
+
+    @Override
+    public void setSee(boolean see) {
+        this.mustSee = see;
+    }
+
+    /**
+     * Whether this entity must reach the target.
+     * @return true if entity must reach target, else false
+     */
+    public boolean mustReach() {
+        return this.reach;
+    }
+
+    /**
+     * Sets whether this entity must reach the target.
+     * @param reach true if entity must reach target, else false
+     */
+    public void setReach(boolean reach) {
+        this.reach = reach;
+    }
 
 }
