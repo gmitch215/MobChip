a07511da2bbee5e8112719e774e5840a1a0faf3c
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-07-04 20:51:07 -0500

    Update 1.17 Abstraction
    - Move Axolotl Behavior

---
+++ 1_17_R1/src/main/java/me/gamercoder215/mobchip/bukkit/BukkitAxolotlBehavior.java
@@ -0,0 +1,21 @@
+package me.gamercoder215.mobchip.bukkit;
+
+import me.gamercoder215.mobchip.ai.behavior.AxolotlBehavior;
+import me.gamercoder215.mobchip.ai.behavior.BehaviorResult;
+import org.bukkit.entity.Axolotl;
+import org.jetbrains.annotations.NotNull;
+
+class BukkitAxolotlBehavior extends BukkitCreatureBehavior implements AxolotlBehavior {
+
+    final Axolotl m;
+
+    public BukkitAxolotlBehavior(Axolotl a) {
+        super(a);
+        this.m = a;
+    }
+
+    @Override
+    public @NotNull BehaviorResult playDead() {
+        return run("PlayDead");
+    }
+}
