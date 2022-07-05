ae406f3f9a6750191b5ee9c16475ac918f4881b7
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-07-04 20:52:13 -0500

    Update Abstraction
    - Move Bukkit Behaviors
    - Update ChipUtil

--- abstraction/src/main/java/me/gamercoder215/mobchip/abstraction/ChipUtil.java
+++ abstraction/src/main/java/me/gamercoder215/mobchip/abstraction/ChipUtil.java
@@ -1,207 +1,96 @@
 package me.gamercoder215.mobchip.abstraction;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.PathfinderMob;
-import net.minecraft.world.entity.ai.attributes.RangedAttribute;
-import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.level.Level;
-import org.bukkit.*;
-import org.bukkit.attribute.Attribute;
-import org.bukkit.entity.*;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.inventory.ItemStack;
+import me.gamercoder215.mobchip.EntityBody;
+import me.gamercoder215.mobchip.ai.behavior.BehaviorResult;
+import me.gamercoder215.mobchip.ai.controller.EntityController;
+import me.gamercoder215.mobchip.ai.goal.CustomPathfinder;
+import me.gamercoder215.mobchip.ai.goal.Pathfinder;
+import me.gamercoder215.mobchip.ai.goal.WrappedPathfinder;
+import me.gamercoder215.mobchip.ai.memories.Memory;
+import me.gamercoder215.mobchip.ai.navigation.EntityNavigation;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Mob;
 
-import java.lang.reflect.Method;
-import java.util.List;
-import java.util.logging.Logger;
-
-import static org.bukkit.event.entity.EntityDamageEvent.DamageCause.*;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Represents an Abstract Wrapper
  */
 public interface ChipUtil {
 
-    // Conversions
+    void addCustomPathfinder(CustomPathfinder p, int priority, boolean target);
 
-    ItemStack convert(net.minecraft.world.item.ItemStack item);
+    // Other
+    Set<WrappedPathfinder> getGoals(Mob m, boolean target);
 
-    net.minecraft.world.item.ItemStack convert(ItemStack item);
+    Collection<WrappedPathfinder> getRunningGoals(Mob m, boolean target);
 
-    SoundEvent convert(Sound s);
+    void setFlag(Mob m, Pathfinder.PathfinderFlag flag, boolean target, boolean value);
 
-    Sound convert(SoundEvent s);
+    void addPathfinder(Pathfinder p, int priority, boolean target);
 
-    ServerPlayer convert(Player p);
+    BehaviorResult runBehavior(Mob m, String behaviorName, Object... args);
 
-    net.minecraft.world.entity.monster.hoglin.Hoglin convert(Hoglin p);
+    BehaviorResult runBehavior(Mob m, String behaviorName, String packageName, Object... args);
 
-    net.minecraft.world.entity.monster.piglin.Piglin convert(Piglin p);
+    EntityController getController(Mob m);
 
-    AgeableMob convert(Ageable a);
+    EntityNavigation getNavigation(Mob m);
 
-    ServerLevel convert(World w);
+    EntityBody getBody(Mob m);
 
-    List<net.minecraft.world.entity.LivingEntity> convert(List<LivingEntity> list);
+    <T> void setMemory(Mob mob, Memory<T> m, T value);
 
-    net.minecraft.world.entity.Entity convert(Entity en);
+    <T> void setMemory(Mob mob, Memory<T> m, T value, long durationTicks);
 
-    net.minecraft.world.entity.Mob convert(Mob m);
+    <T> T getMemory(Mob mob, Memory<T> m);
 
-    net.minecraft.world.entity.LivingEntity convert(LivingEntity en);
+    long getExpiry(Mob mob, Memory<?> m);
 
-    ItemEntity convert(Item i);
+    boolean contains(Mob mob, Memory<?> m);
 
-    PathfinderMob convert(Creature c);
+    void removeMemory(Mob mob, Memory<?> m);
 
-    ResourceLocation convert(NamespacedKey key);
+    boolean isRestricted(Mob m);
 
-    RangedAttribute convert(Attribute a);
+    void clearRestriction(Mob m);
 
-    boolean exists(NamespacedKey key, Registry<?> registry);
+    void restrictTo(Mob m, double x, double y, double z, int radius);
 
-    // Defaults (Requires no CraftBukkit)
+    Location getRestriction(Mob m);
 
-    default DamageSource convert(EntityDamageEvent.DamageCause c) {
-        return switch (c) {
-            case FIRE -> DamageSource.IN_FIRE;
-            case LIGHTNING -> DamageSource.LIGHTNING_BOLT;
-            case FIRE_TICK -> DamageSource.ON_FIRE;
-            case SUFFOCATION -> DamageSource.IN_WALL;
-            case LAVA -> DamageSource.LAVA;
-            case HOT_FLOOR -> DamageSource.HOT_FLOOR;
-            case CRAMMING -> DamageSource.CRAMMING;
-            case DROWNING -> DamageSource.DROWN;
-            case STARVATION -> DamageSource.STARVE;
-            case CONTACT -> DamageSource.CACTUS;
-            case MAGIC -> DamageSource.MAGIC;
-            case FALL -> DamageSource.FALL;
-            case FLY_INTO_WALL -> DamageSource.FLY_INTO_WALL;
-            case VOID -> DamageSource.OUT_OF_WORLD;
-            case WITHER -> DamageSource.WITHER;
-            case FALLING_BLOCK -> DamageSource.FALLING_BLOCK;
-            case DRAGON_BREATH -> DamageSource.DRAGON_BREATH;
-            case FREEZE -> DamageSource.FREEZE;
-            case DRYOUT -> DamageSource.DRY_OUT;
-            default -> DamageSource.GENERIC;
-        };
-    }
+    int getRestrictionRadius(Mob m);
 
-    default LivingEntity convert(net.minecraft.world.entity.LivingEntity en) {
-        return (LivingEntity) en.getBukkitEntity();
-    }
+    boolean hasRestriction(Mob m);
 
-    default Mob convert(net.minecraft.world.entity.Mob mob) {
-        return (Mob) mob.getBukkitEntity();
-    }
+    boolean canSee(Mob m, Entity en);
 
-    default Creature convert(PathfinderMob mob) {
-        return (Creature) mob.getBukkitEntity();
-    }
-
-    default Ageable convert(AgeableMob mob) { return (Ageable) mob.getBukkitEntity(); }
-
-    default Entity convert(net.minecraft.world.entity.Entity en) { return en.getBukkitEntity(); }
-
-    default net.minecraft.world.entity.EntityType<?> convert(org.bukkit.entity.EntityType type) {
-        return Registry.ENTITY_TYPE.get(convert(type.getKey()));
-    }
-
-    default BlockPos convert(Location loc) {
-        return new BlockPos(loc.getX(), loc.getY(), loc.getZ());
-    }
-
-    default net.minecraft.world.Difficulty convert(Difficulty d) {
-        return net.minecraft.world.Difficulty.byName(d.name().toLowerCase());
-    }
-
-    default Difficulty convert(net.minecraft.world.Difficulty d) {
+    default void updateGoals(Map<Integer, Pathfinder> goals, Map<Integer, Boolean> target) {
         try {
-            return Difficulty.valueOf(d.getKey().toUpperCase());
-        } catch (IllegalArgumentException e) {
-            return Difficulty.NORMAL;
+            for (Map.Entry<Integer, Pathfinder> e : goals.entrySet())
+                addPathfinder(e.getValue(), e.getKey(), target.get(e.getKey()));
+        } catch (ClassCastException e) {
+            Bukkit.getLogger().severe("[MobChip] Invalid Projectile Source: " + e.getMessage());
+            for (StackTraceElement s : e.getStackTrace())
+                Bukkit.getLogger().severe("[MobChip] " + s.toString());
         }
     }
 
-    default World convert(Level l) {
-        return l.getWorld();
+    static String getServerVersion() {
+        return Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3].substring(1);
     }
 
-    default Location convert(World w, BlockPos pos) {
-        return new Location(w, pos.getX(), pos.getY(), pos.getZ());
-    }
-
-    default Player convert(net.minecraft.world.entity.player.Player p) {
-        return Bukkit.getPlayer(p.getUUID());
-    }
-
-    default EntityDamageEvent.DamageCause convert(DamageSource c) {
-        return switch (c.msgId) {
-            case "inFire" -> FIRE;
-            case "lightningBolt" -> LIGHTNING;
-            case "onFire" -> FIRE_TICK;
-            case "lava" -> LAVA;
-            case "hotFloor" -> HOT_FLOOR;
-            case "inWall" -> SUFFOCATION;
-            case "cramming" -> CRAMMING;
-            case "drown" -> DROWNING;
-            case "starve" -> STARVATION;
-            case "cactus", "sweetBerryBush", "stalagmite" -> CONTACT;
-            case "fall" -> FALL;
-            case "flyIntoWall" -> FLY_INTO_WALL;
-            case "outOfWorld" -> VOID;
-            case "magic" -> MAGIC;
-            case "wither" -> WITHER;
-            case "anvil", "fallingBlock", "fallingStalactite" -> FALLING_BLOCK;
-            case "dragonBreath" -> DRAGON_BREATH;
-            case "dryout" -> DRYOUT;
-            case "freeze" -> FREEZE;
-            default -> CUSTOM;
-        };
-    }
-
-    default Hoglin convert(net.minecraft.world.entity.monster.hoglin.Hoglin h) { return (Hoglin) h.getBukkitEntity(); }
-
-    default Piglin convert(net.minecraft.world.entity.monster.piglin.AbstractPiglin p) { return (Piglin) p.getBukkitEntity(); }
-
-    default <T extends org.bukkit.entity.Entity> Class<? extends T> toBukkitClass(Class<T> bukkit, Class<? extends net.minecraft.world.entity.Entity> clazz) {
+    static ChipUtil getWrapper() {
         try {
-            Method m = clazz.getDeclaredMethod("getBukkitEntity");
-            m.setAccessible(true);
-
-            return m.getReturnType().asSubclass(bukkit);
+            String pkg = ChipUtil.class.getPackage().getName();
+            return (ChipUtil)Class.forName(pkg + ".ChipUtil" + getServerVersion()).getConstructor().newInstance();
         } catch (Exception e) {
-            Logger.getGlobal().severe(e.getMessage());
-            return null;
+            throw new IllegalStateException("Invalid Version: " + getServerVersion());
         }
     }
-
-    default Class<? extends net.minecraft.world.entity.Entity> toNMSClass(Class<? extends Entity> clazz) {
-        try {
-            return clazz.getDeclaredField("entity").getType().asSubclass(net.minecraft.world.entity.Entity.class);
-        } catch (Exception e) {
-            Logger.getGlobal().severe(e.getMessage());
-            return null;
-        }
-    }
-
-    default Class<? extends net.minecraft.world.entity.LivingEntity> toLivingNMSClass(Class<? extends LivingEntity> clazz) {
-        try {
-            return clazz.getDeclaredField("entity").getType().asSubclass(net.minecraft.world.entity.LivingEntity.class);
-        } catch (Exception e) {
-            Logger.getGlobal().severe(e.getMessage());
-            return null;
-        }
-    }
-
-    default Item convert(ItemEntity en) { return (Item) en.getBukkitEntity(); }
-
 
 }
