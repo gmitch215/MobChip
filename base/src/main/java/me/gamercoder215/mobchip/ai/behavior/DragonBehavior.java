9b2b2b56d9615ee407f18a90e4681e3ae77786c0
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-07-21 23:31:58 -0500

    Create DragonBehavior.java
    DragonBehavior Draft API

---
+++ base/src/main/java/me/gamercoder215/mobchip/ai/behavior/DragonBehavior.java
@@ -0,0 +1,22 @@
+package me.gamercoder215.mobchip.ai.behavior;
+
+import org.bukkit.entity.Entity;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+/**
+ * Represents Entity Behaviors for an Ender Dragon
+ */
+public interface DragonBehavior extends CreatureBehavior {
+
+    /**
+     * Makes this Ender Dragon perform a natural knockback with its wings when Entities get too close.
+     * @param entities List of Entities to knock back
+     * @return Result of Behavior
+     */
+    @NotNull
+    BehaviorResult naturalKnockback(@Nullable List<Entity> entities);
+
+}
