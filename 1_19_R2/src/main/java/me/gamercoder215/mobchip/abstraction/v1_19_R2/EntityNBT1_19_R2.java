5dad7afe76e167f658e308ccf607a6dd772082b5
datatags <datatags@users.noreply.github.com>
2022-12-08 11:55:10 -0800

    Copy 1_19_R1 -> 1_19_R2 and rename

---
+++ 1_19_R2/src/main/java/me/gamercoder215/mobchip/abstraction/v1_19_R2/EntityNBT1_19_R2.java
@@ -0,0 +1,28 @@
+package me.gamercoder215.mobchip.abstraction.v1_19_R2;
+
+import me.gamercoder215.mobchip.abstraction.ChipUtil1_19_R2;
+import me.gamercoder215.mobchip.nbt.EntityNBT;
+import net.minecraft.nbt.CompoundTag;
+import org.bukkit.entity.Mob;
+import org.jetbrains.annotations.NotNull;
+
+public final class EntityNBT1_19_R2 extends NBTSection1_19_R2 implements EntityNBT {
+
+    private final Mob mob;
+    private final net.minecraft.world.entity.Mob handle;
+
+    private final CompoundTag root;
+
+    public EntityNBT1_19_R2(Mob m) {
+        super(m);
+        this.mob = m;
+        this.handle = ChipUtil1_19_R2.toNMS(m);
+        this.root = new CompoundTag();
+        handle.saveWithoutId(root);
+    }
+
+    @Override
+    public @NotNull Mob getEntity() {
+        return mob;
+    }
+}
