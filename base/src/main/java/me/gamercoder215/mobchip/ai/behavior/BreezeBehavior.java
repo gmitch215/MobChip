c8f3afaa5fa75d980adc9884e3d87ba0e9e5c65d
Gregory Mitchell <gamercoder215@gmail.com>
2023-12-06 17:22:07 +0000

    Add Breeze Behavior

---
+++ base/src/main/java/me/gamercoder215/mobchip/ai/behavior/BreezeBehavior.java
@@ -0,0 +1,28 @@
+package me.gamercoder215.mobchip.ai.behavior;
+
+import me.gamercoder215.mobchip.ai.schedule.Updatable;
+
+/**
+ * Represents Behavior for a Breeze
+ */
+public interface BreezeBehavior extends CreatureBehavior, Updatable {
+    
+    /**
+     * Performs a Breeze Long Jump.
+     * <p>This behavior requires {@link EntityMemory#ATTACK_TARGET} to be present in the brain.</p>
+     */
+    void longJump();
+
+    /**
+     * Performs a Breeze Shooting Attack.
+     * <p>This behavior requires {@link EntityMemory#ATTACK_TARGET} to be present in the brain.</p>
+     */
+    void shoot();
+
+    /**
+     * Performs a Breeze Slide.
+     * <p>This behavior requires {@link EntityMemory#ATTACK_TARGET} to be present in the brain.</p>
+     */
+    void slide();
+
+}
