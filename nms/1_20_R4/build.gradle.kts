21a4b216589be9766ae2df88f57e254e9205e19d
Gregory Mitchell <grmitchell713@gmail.com>
2024-04-24 18:43:56 +0000

    Draft 1.20.5 Support

---
+++ nms/1_20_R4/build.gradle.kts
@@ -0,0 +1,41 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import io.github.patrick.gradle.remapper.RemapTask
+
+plugins {
+    id("io.github.patrick.remapper") version "1.4.0"
+}
+
+val mcVersion = "1.20.5"
+
+dependencies {
+    api(project(":mobchip-base"))
+    api(project(":mobchip-abstraction"))
+    api(project(":mobchip-1_14_R1"))
+
+    compileOnly("org.spigotmc:spigot:$mcVersion-R0.1-SNAPSHOT:remapped-mojang")
+    testImplementation("org.spigotmc:spigot:$mcVersion-R0.1-SNAPSHOT:remapped-mojang")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks {
+    assemble {
+        dependsOn("remap")
+    }
+
+    jar.configure {
+        enabled = false
+    }
+
+    remap {
+        dependsOn("shadowJar")
+
+        inputTask.set(getByName<ShadowJar>("shadowJar"))
+        version.set(mcVersion)
+        action.set(RemapTask.Action.MOJANG_TO_SPIGOT)
+        archiveName.set("${project.name}-${project.version}.jar")
+    }
+}
